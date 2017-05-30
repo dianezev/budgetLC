c2e000a037bf890fa7591ee02ca7a4576f1d8bf7
Daniel Clough <daniel.alan.clough@gmail.com>
2017-05-30 11:53:12 -0700

    class files

---
+++ php/inc/constants.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'db_user');
+	define('DB_PASS', 'db_password');
+	define('DB_NAME', 'db_name');
+	
+    // HTML Whitelist
+    define('WHITELIST', '<b><i><strong><em><a>');
+
+?>
