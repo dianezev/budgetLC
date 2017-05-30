f79e48ab4253c098eea39bd6f09499dcb4fb9654
Daniel Clough <daniel.alan.clough@gmail.com>
2017-05-30 13:15:59 -0700

    Update accountVerify.php

--- php/accountVerify.php
+++ php/accountVerify.php
@@ -6,13 +6,13 @@
     if(isset($_GET['v']) && isset($_GET['e']))
     {
         include_once "inc/class.users.inc.php";
-        $users = new ColoredListsUsers($db);
+        $users = new FinanceUsers($db);
         $ret = $users->verifyAccount();
     }
     elseif(isset($_POST['v']))
     {
         include_once "inc/class.users.inc.php";
-        $users = new ColoredListsUsers($db);
+        $users = new FinanceUsers($db);
         $ret = $users->updatePassword();
     }
     else
