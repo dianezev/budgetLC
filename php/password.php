efabbc154c50d7865aa8791144e21e552f0cb68e
Daniel Clough <daniel.alan.clough@gmail.com>
2017-06-07 23:31:29 -0700

    Create password.php

--- php/password.php
+++ php/password.php
@@ -7,12 +7,12 @@
 		<p>Enter the email address you signed up with and we'll send 
 		you a link to reset your password.</p>
 
-		<form action="php/db-interaction/users.php" method="post">
+		<form action="db-interaction/users.php" method="post">
 			<div>
 				<input type="hidden" name="action"
 					value="resetpassword" />
-				<input type="text" name="username" id="username" />
-				<label for="username">Email</label><br /><br />
+				<label for="username">Email</label>
+				<input type="text" name="username" id="username" /><br /><br />
 				<input type="submit" name="reset" id="reset"
 					value="Reset Password" class="button" />
 				<input type="hidden" name="token"
