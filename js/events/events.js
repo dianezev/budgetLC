ec52b815d6fb49112403925949be8dde48452573
Diane Zevenbergen <dianezev@comcast.net>
2017-06-12 19:22:12 -0700

    ui login stuff2

--- js/events/events.js
+++ js/events/events.js
@@ -1,6 +1,11 @@
 // Display login modal window
 $('.toLogin').on('click', function() {
-  $('#login').toggle();
+
+  if (this.text === "LOG OUT") {
+    LCB.controller.signout();
+  } else {
+    $('#login').toggle();
+  }
 });
 
 // Close login window
@@ -8,25 +13,31 @@ $('#login span').on('click', function() {
   $('#login').toggle();
 });
 
+// Create account or signin
+// DMZ question: not sure if pw should be hashed here or if plain
+// pw should be passed to model for hashing...
+$('#login button').on('click', function(e) {
+  var id = this.id;
+  var userInfo = {};
+
+  if (id === "signin_first") {
+    userInfo.name = $('#name_set').val();
+    userInfo.pw = $('#password_set').val();
+    userInfo.email = $('#email_set').val();
+
+  } else if (id === "signin_return") {
+    userInfo.name = '';
+    userInfo.email = $('#email').val();
+    userInfo.pw = $('#password').val();
+  }
+
+  LCB.controller.signin(userInfo);
+});
+
+
 // For login - toggling between Sign Up and Log In
 $('.message a').click(function(){
    $('#login form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
-
-// Create new login
-$('#signin_first').on('click', function() {
-  // stuff for creating login
-  
-  // if successful close login window
-  $('#login').toggle();
-});
-
-// Verify sign in info
-$('#signin_return').on('click', function() {
-  // stuff to verify sign in
-  
-  // if successful close login window
-  $('#login').toggle();
 });
 
 // Toggle between showing and hiding the sidebar when clicking the menu icon
