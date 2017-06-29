2f248e1f332c34d518aed534b438f36d4d6d037c
Daniel Clough <daniel.alan.clough@gmail.com>
2017-06-28 23:17:42 -0700

    Add files via upload

---
+++ php/checkUrl.php
@@ -0,0 +1,17 @@
+<?php
+  // called by AJAX in model.checkUrl
+  
+  include_once "../include/class.users.php";
+
+  include_once "../common/base.php";
+  
+  $e = $_POST["e"];
+  $v = $_POST["v"];
+
+  $userObj = new Users($db);
+
+  $logObj = $userObj->verifyAccount();
+
+  echo JSON_encode($logObj);
+
+?>
