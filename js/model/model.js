d28805f4f01061a8974707d4a31809627b9fd841
Daniel Clough <daniel.alan.clough@gmail.com>
2017-06-28 18:58:00 -0700

    Update model.js

--- js/model/model.js
+++ js/model/model.js
@@ -5,42 +5,46 @@ LCB = window.LCB || {};
 LCB.model = (function() {
   'use strict';
   
-  // Private vars here
-  var user = {email: '', name: ''};
-  
+  // Private vars here  
   
   // Public vars & functions here
   var publicAPI = {
     
     // Public vars here
     //
+    user = {email: '', name: '', e:'', v:''};
 
     // Public functions here
     
-    signup: function(info, cb) {
-      var email = info.email;
-      var name = info.name;
+    checkUrl: function(urlInfo, cb) {
+      var that = this;
       
       cb = cb || function () {};
       
-      // TBD: Check email format here rather than doing that in php? faster, I think
-      
-      // If email format is OK...
       $.ajax({
         method: "POST",
-        data: {email, name},
-        url: "/test/php/api/signup.php",
+        data: {e: urlInfo.e, v: urlInfo.v},
+        url: "/test/php/api/checkUrl.php",
         success: function(result){
-          cb($.parseJSON(result));
+          result = $.parseJSON(result);
+
+          // Update variables if login was successful
+          if (result.hasOwnProperty('user')) {
+            that.user.email = result.user.email;
+            that.user.name = result.user.name;
+          }
+
+          cb(result);
         },
         error: function(xhr, status, error) {
           alert('ajax ERROR: ' + error);
         }
       });      
     },
-    login: function(info, cb) {
-      var email = info.email;
-      var password = info.password;
+    login: function(userInfo, cb) {
+      var email = userInfo.email;
+      var password = userInfo.password;
+      var that = this;
 
       cb = cb || function () {};
 
@@ -50,13 +54,12 @@ LCB.model = (function() {
         data: {email, password},
         url: "/test/php/api/login.php",
         success: function(result){
-          console.log(result);
           result = $.parseJSON(result);
           
           // Update variables if login was successful
-          if (typeof result.name !== "undefined") {
-            user.email = result.email;
-            user.name = result.name;
+          if (result.hasOwnProperty('user')) {
+            that.user.email = result.user.email;
+            that.user.name = result.user.name;
           }
           cb(result);
         },
@@ -65,12 +68,48 @@ LCB.model = (function() {
         }
       });      
     },
-    signout: function() {
+    logout: function() {
         this.user = {email: '', name: '', hash: ''};
 
       // other db/backend processing called here
 
-    }
+    },
+      setPassword: function(userInfo, cb) {
+      cb = cb || function () {};
+      
+      $.ajax({
+        method: "POST",
+        data: {v: userInfo.v, password: userInfo.password},
+        url: "/test/php/api/setPassword.php",
+        success: function(result){
+          cb($.parseJSON(result));
+        },
+        error: function(xhr, status, error) {
+          alert('ajax ERROR: ' + error);
+        }
+      });      
+    },
+    signup: function(userInfo, cb) {
+      var email = userInfo.email;
+      var name = userInfo.name;
+      
+      cb = cb || function () {};
+      
+      // TBD: Check email format here rather than doing that in php? faster?
+      
+      // If email format is OK...
+      $.ajax({
+        method: "POST",
+        data: {email, name},
+        url: "/test/php/api/signup.php",
+        success: function(result){
+          console.log(result);
+          cb($.parseJSON(result));
+        },
+        error: function(xhr, status, error) {
+          alert('ajax ERROR: ' + error);
+        }
+      });
 
   };
   return publicAPI;
