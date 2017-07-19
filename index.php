e63296d89f326f464cebbfca8a65c0bf10a1973d
Michael Park <thatmichaelpark@gmail.com>
2017-07-19 16:51:16 -0700

    test change

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<title>Total Finance</title>
+<title>Total Finance(!)</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
