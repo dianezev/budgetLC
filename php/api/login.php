c07fad68fecf8f026ca7eecf4e4861e34b147310
Michael Park <thatmichaelpark@gmail.com>
2017-08-08 15:00:11 -0700

    CORS fix

--- php/api/login.php
+++ php/api/login.php
@@ -4,6 +4,8 @@
   include_once "../include/class.users.php";
 
   include_once "../common/base.php";
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Methods: *");
   header("Content-Type: application/json");
   $email = $_POST["email"];
   $password = $_POST["password"];
